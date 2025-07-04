6542f7e07ea97728019ede503fd91c5b045b5b27
Michael J. Oria <michael_oria@me.com>
2025-07-04 14:34:23 -0400

    Updated with the tiniest (silliest) change.

--- Sql/StoredProcedures/usp_SPA_getStockResults.sql
+++ Sql/StoredProcedures/usp_SPA_getStockResults.sql
@@ -5,7 +5,7 @@ BEGIN
         s.Ticker 'Symbol'
     ,   CONCAT(ROUND(((f.ClosingPrice - s.[Close]) / s.[Close] * 100), 2), '%') 'PercentageDifference'
     ,   CASE WHEN f.IsSuccessful = 1 THEN 'GREEN' ELSE 'RED' END 'Result'
-	,   FORMAT(f.DateUpdated, 'MM-dd-yy hh:mm:ss') 'DateTime'
+    ,   FORMAT(f.DateUpdated, 'MM-dd-yy hh:mm:ss') 'DateTime'
     FROM SPA_StockSetups s
     JOIN SPA_FinalResults f ON s.Id = f.StockSetupId
 END;
