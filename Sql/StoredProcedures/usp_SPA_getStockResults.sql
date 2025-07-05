acafde8fd200b46d77d464a82afc9a9f445a1a3a
Michael J. Oria <michael_oria@me.com>
2025-07-05 15:19:18 -0400

    resolved a formatting issue.

--- Sql/StoredProcedures/usp_SPA_getStockResults.sql
+++ Sql/StoredProcedures/usp_SPA_getStockResults.sql
@@ -3,10 +3,10 @@ CREATE OR ALTER PROCEDURE usp_SPA_getFinalResults
 AS
 BEGIN
     SELECT
-		  s.Ticker AS Ticker,
-		  ROUND(((f.ClosingPrice - s.[Close]) / s.[Close] * 100), 2) AS PercentageDifference, -- Return as numeric
-		  CASE WHEN f.ClosingPrice > s.[Close] THEN 'GREEN' ELSE 'RED' END AS GreenOrRedDay,
-		  CONVERT(VARCHAR(17), f.DateUpdated, 13) AS DateUpdated
+	      s.Ticker AS Ticker,
+	      ROUND(((f.ClosingPrice - s.[Close]) / s.[Close] * 100), 2) AS PercentageDifference,
+	      CASE WHEN f.ClosingPrice > s.[Close] THEN 'GREEN' ELSE 'RED' END AS GreenOrRedDay,
+	      CONVERT(VARCHAR(17), f.DateUpdated, 13) AS DateUpdated
 	FROM  SPA_StockSetups s
 	JOIN  SPA_FinalResults f ON s.Id = f.StockSetupId
 	WHERE s.IsFinalized = 1
